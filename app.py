79883eb3f5b3dccb0809933edb14be7f265bf244
developer <hilman@localhost.com>
2025-11-26 09:36:59 +0700

    move to app

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello World testing</p>"
+
+@app.route("/country/")
+def getCountry():
+  url = 'https://restcountries.com/v3.1/all'
+  r = requests.get(url)
+  return r.content
+
+@app.route("/country/<name>")
+def getCountryByName(name):
+  url = 'https://restcountries.com/v3.1/name/'+name
+  r = requests.get(url)
+  return r.content
